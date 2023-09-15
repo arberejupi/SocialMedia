d44e48208bfef1f9226cee448741c5d36571384f
arberejupi <73881615+arberejupi@users.noreply.github.com>
2023-09-15 19:04:57 +0200

    Add files via upload

--- SocialMediaApplication/Models/Post.cs
+++ SocialMediaApplication/Models/Post.cs
@@ -1,4 +1,6 @@
-﻿namespace SocialMediaApplication.Models
+﻿using System.Text.Json.Serialization;
+
+namespace SocialMediaApplication.Models
 {
     public class Post
     {
@@ -6,5 +8,6 @@
         public int UserId { get; set; }
         public string Content { get; set; }
         public DateTime CreatedAt { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
